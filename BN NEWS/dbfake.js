dad8171c4b7180a50ced92a1c05040fe4074b2cb
BelleNerissa <49173787+BelleNerissa@users.noreply.github.com>
2019-06-04 20:59:35 -0300

    Add files via upload

---
+++ BN NEWS/dbfake.js
@@ -0,0 +1,35 @@
+var db = {
+    dados: [
+        {
+            id: 0,
+            titulo: 'Teoria Louca de fãns ganha muitos apoiadores em uma nova petição de 5600 assinaturas',
+            descricao: 'Muito vem sendo cogitado sobre como a equipe dos Vingadores destruirá o vilão Thanos em Ultimato. Porém, um grupo de fãs da Marvel parece ter conseguido uma solução bem simples (e não tão agradável visualmente).',
+            imagem: ' avengers.jpg ',
+            corpo: 'Os brasileiros não tem limites na Internet!Muito vem sendo cogitado sobre como a equipe dos Vingadores destruirá o vilão Thanos em Ultimato. Porém, um grupo de fãs da Marvel parece ter conseguido uma solução bem simples (e não tão agradável visualmente).A petição quer chegar em 7.500 assinaturas e até o momento em que esta notícia foi redigida já tinha mais de 6.800. Infelizmente – ou felizmente – o filme já está gravado e editado e a petição não fará a Marvel mudar de ideia.O caótico curso de eventos iniciado por Thanos,que dizimou metade do universo e fragmentou o time conhecido como Vingadores, leva os heróis sobreviventes a lutar uma última vez nessa grande conclusão de 22 filmes, ‘Vingadores: Ultimato’.Vingadores: Ultimato estreia em 25 de Abril de 2019, e é dirigido pelos irmãos Anthony e Joe Russo. Vingadores: Guerra Infinita arrecadou mais de US$ 2 bilhões para a Disney e para a Marvel Studios.',
+            categoria: 'entreterimento',
+            data: 30/04/2019,
+            link:  'Main New.html',
+            origem: 'http://pipocasclub.com.br/2019/03/26/vingadores-ultimato-peticao-online-anus-do-thanos/',
+        }, {
+            id: 1,
+            titulo: 'Confira as atrações e novidades confirmadas no Rock in Rio 2019',
+            descricao: 'O festival Rock in Rio 2019 está confirmado no Brasil. As atrações da programação estão sendo divulgadas aos poucos.Confirmados: Anitta, Iron Maiden, Sepultura, Scorpions, Megadeth, Black Eyed Peas, P!nk, Imagine Dragon, Red Hot Chili Peppers, e mais... ',
+            imagem: ' Rock-In-Rio.jpg ',
+            corpo: 'O festival Rock in Rio 2019 está confirmado no Brasil. As atrações da programação estão sendo divulgadas aos poucos.Os dias confirmados para realização do evento em 2019, ficam entre 27 de setembro e 6 de outubro. O horário das apresentações deve ser divulgado em breve .Uma grande inovação para a nova edição do Rock in Rio será o espaço Favela.O palco recriará o ambiente de uma favela, grandiosa, lúdica e bem colorida. Além de muita música, o espaço receberá talentos da dança e do teatro pertencentes a comunidade da Rocinha.A cantora  Anitta  foi a primeira confirmação do lineup de atrações do Rock in Rio em 2019.As bandas Iron Maiden, Sepultura, Scorpions e Megadeth também foram confirmadas para alegria do público metaleiro. As bandas irão se apresentar no dia 04 de outubro, denominado “Dia do Metal” ,“Quis garantir logo o Iron Maiden. Além de ser uma das mais importantes bandas de metal do planeta, o grupo tem uma relação profunda com o público brasileiro e queríamos renovar esse laço.Scorpions e Megadeth têm fãs muito fieis aqui, além de trajetórias longas e respeitadas. E o Sepultura é a maior banda de metal do Brasil”, disse o diretor do Rock in Rio 2019, Roberto Medina. O grupo de hip-hop Black Eyed Peas formado em Los Angeles, Califórnia, também está confirmado no Rock in Rio 2019. Desde seu terceiro álbum Elephunk em 2003, o grupo já vendeu mais de 35 milhões de álbuns e 41 milhões de singles.Outra grande atração que está oficialmente confirmada é a cantora P!nk, famosa por seus sucessos no ritmo pop rock. Aos 39 anos, três Grammys e mais de 60 milhões de álbuns vendidos, a artistas fará sua aguardada estreia na América Latina.As bandas Imagine Dragon e Nickelback também já estão com a presença garantida na nova edição do Rock in Rio 2019. E para alegria do público, mais 7 atrações foram confirmadas recentemente, são elas as bandas de rock Slayer e Anthrax, Jessie J, Charlie Puth, King Crimson, o canadense Drake, e os artistas nacionais Lulu Santos e Silva.A aclamada banda de rock Red Hot Chili Peppers também foi confirmada para trazer bastante força a programação e energia ao público. Outra grande atração também anunciada foi o artistas Dave Mathews.',
+            categoria: 'eventos',
+            data: 30/04/2019,
+            link: 'news1.htm',
+            origem: 'https://oglobo.globo.com/cultura/veja-programacao-completa-do-rock-in-rio-2019-23566329',
+        }, {
+            id: 2,
+            titulo: 'Vacina para rinite descoberta por brasileiros',
+            descricao: 'Boa notícia para o verdadeiro batalhão que sofre com rinite: um pesquisador brasileiro descobriu uma vacina capaz de reduzir drasticamente os sintomas.',
+            imagem: 'vacina-para-rinite.jpg',
+            corpo: 'Boa notícia para o verdadeiro batalhão que sofre com rinite: um pesquisador brasileiro descobriu uma vacina capaz de reduzir drasticamente os sintomas. De acordo com a Organização Mundial da Saúde (OMS), mais de 500 milhões têm rinite no mundo. No Brasil, segundo a Associação Brasileira de Alergia e Imunologia (Asbai), a rinite ataca 30% dos adolescentes e 26% das crianças. Agora, com o tratamento a longo prazo descoberto por um professor da Faculdade de Medicina de Jundiaí, será possível dar mais qualidade de vida a essas pessoas. Após 10 anos de estudos sobre a rinite alérgica, um professor titular de Otorrinolaringologia da Faculdade de Medicina de Jundiaí, o pesquisador Edmir Américo Lourenço, encontrou um tratamento a longo prazo contra a doença. Segundo ele, a vacina não cura, mas é capaz de reduzir tão drasticamente os sintomas da rinite que parece que o indivíduo está curado.Para surtir o efeito esperado, no entanto, a vacina deve ser individualizada, elaborada a partir de testes indicativos dos componentes aos quais a pessoa é sensível. Os mais comuns são ácaros, fungos, penas, poeira, pólen ou pelos de animaisA partir daí a vacina é elaborada de forma individual e específica, de forma a estimular a formação de anticorpos específicos contra aqueles componentes. O tratamento é composto por 30 doses aplicadas durante 1 ano e 2 meses.Aos poucos o tratamento vai melhorando a qualidade de vida do portador, assim como a qualidade do sono, a capacidade de trabalho e até o humor. No entanto, o pesquisador avisa que, como não é uma cura, a pessoa não deve ser exposta a condições extremas.',
+            categoria: 'saude',
+            data: 30/04/2019,
+            link:'news2.html',
+            origem: 'http://g1.globo.com/sao-paulo/sorocaba-jundiai/noticia/2016/05/medico-apresenta-solucao-contra-rinite-alergica-apos-10-anos-de-estudo.html',
+        }
+    ]
+}
